2242c792ce460f8f02723fbd92ba73802e0bbea2
Endel Dreyer <endel.dreyer@gmail.com>
2016-10-09 10:10:01 +0200

    fix issue on vanilla source. update results

--- vanilla/index.js
+++ vanilla/index.js
@@ -16,14 +16,13 @@ Level1.prototype.method = function() {
   Level0.prototype.method.call(this);
   this.num += 1;
 }
-
 Level1.prototype = new Level0();
 Level1.constructor = Level1;
 module.exports.Level1 = Level1;
 
 // Level 2
 function Level2 () {
-  Level1.prototype.method.call(this);
+  Level1.call(this);
   this.num += 1;
 }
 Level2.prototype.method = function() {
